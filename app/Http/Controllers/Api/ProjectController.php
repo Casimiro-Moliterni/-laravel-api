8c4fe6f7d6259161f78bd8325af5a4422ef9ddc6
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-06-11 16:32:04 +0200

    feature add new route for function show for singlepost in projectcontroller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,16 +5,36 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+
 class ProjectController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
 
-        $projects= Project::with('type','technologies')->paginate(3);
+        $projects = Project::with('type', 'technologies')->paginate(3);
 
-       return response()->json([
-        'success'=>'true',
-        'results'=>$projects
-       ]);
+        return response()->json([
+            'success' => 'true',
+            'results' => $projects
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', '=', $slug)->with('type', 'technologies')->first();
+
+        if ($project) {
+            $data = [
+                'success' => 'true',
+                'results' => $project
+            ];
+        } else {
+            $data = [
+                'success' => 'false',
+                'results' => 'Not Found'
+            ];
+        }
+        return response()->json($data);
     }
 }
