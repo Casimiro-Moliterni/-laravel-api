3151b90cda3913c1255af343a3e426e629923be0
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-06-13 15:27:23 +0200

    add validator

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -5,15 +5,33 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Lead;
+use Illuminate\Support\Facades\Validator;
+
 class LeadController extends Controller
 {
     //
 
-    public function store(Request $request){
-        $data=$request->all();
-        $newLead= new Lead();
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'firstname' => 'required|min:3|max:255',
+            'lastname' => 'required|min:3|max:255',
+            'email' => 'required|email',
+            'content' => 'required|min:3|max:500',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success'=>false,
+                'error'=>$validator->errors()
+            ]);
+        }
+        $newLead = new Lead();
         $newLead->fill($data);
         $newLead->save();
-        dd($data);
+        
+        return response()->json([
+        'success'=>true
+        ]);
     }
 }
